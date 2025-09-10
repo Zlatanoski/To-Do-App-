4d13387f340155a5fabbb7023339770790a70fb9
zlatanoski <zlatanoskidavid92@gmail.com>
2025-09-10 03:36:33 +0200

    Register a new user endpoint works and saves user together with its hashed password in local postgresql DB

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,10 +5,40 @@ import prisma from '../prismaClient.js'
 
 const router = express.Router();
 
-router.post('/register',(req,res)=>{
+
+// Register a new user endpoint /auth/register path 
+router.post('/register',async (req,res)=>{
+
+    const {username,password} = req.body
+  
+    const hashedPassword = bcrypt.hashSync(password,8)
+    
+    //Using try catch block to save new user and hashed pasw to DB  
+    try{
+
+        const user = await prisma.user.create({
+            //The first field ID is autoincremented and set 
+            data:{
+            username,
+            password:hashedPassword
+            }
+        })
+
+        //create a token
+        const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'24h'}) 
+
+        res.json({token})
+        
+
+    }catch(error){
+        console.log(error)
+        res.sendStatus(503)
+    }
+
+ 
 
 })
-
+// Login a user endpoint /auth/login path
 router.post('/login',(req,res) =>{
 
 
