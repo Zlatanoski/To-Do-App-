c541ad1c49d2bfe5698e61deafaa8fab3ad009a4
zlatanoski <zlatanoskidavid92@gmail.com>
2025-09-11 02:05:42 +0200

    Login a user works and creates a token for the user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import prisma from '../prismaClient.js'
 
@@ -31,7 +31,7 @@ router.post('/register',async (req,res)=>{
         
 
     }catch(error){
-        console.log(error)
+        console.log(error.message)
         res.sendStatus(503)
     }
 
@@ -39,7 +39,37 @@ router.post('/register',async (req,res)=>{
 
 })
 // Login a user endpoint /auth/login path
-router.post('/login',(req,res) =>{
+router.post('/login',async (req,res) =>{
+
+    //get the email and check the password asssociated with that email in the DB 
+    //we have to one way encrypt and compare the password the user entered 
+
+    const {username,password} = req.body 
+
+    try{
+        //geting the user
+        const user = await prisma.user.findUnique({
+            where:{
+                username:username
+            }
+        })
+        //checking if such user exists if so then proceed on password check 
+        if(!user){return res.status(404).send({message:"User not found!"})}
+        //hash-compare the password with the users hashed password in the DB
+        const isValidPassword = bcrypt.compareSync(password,user.password)
+        if(!isValidPassword) {return res.status(401).send({message:"Invalid password!"})}
+        
+        console.log(user)
+        //if we pass the password check then successfull auth
+        //since user is authenticated need to give them a token so they can perform crud actions
+
+        const token = jwt.sign({id:user.id}, process.env.JWT_SECRET, {expiresIn:'24h'})
+        res.json({token})
+
+    }catch(error){
+        console.log(error.message)
+        res.sendStatus(503) // sending error in the backend code 503
+    }
 
 
 })
